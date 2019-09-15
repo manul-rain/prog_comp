9a9043a1df1ed114920bb0bdaad769798143cebf
manul-rain <kdr71.nkn32@gmail.com>
2019-09-16 05:22:58 +0900

    add BeginnerContest051

---
+++ VisualStudioCode/AtCoder/BeginnerContest051/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BeginnerContest051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer=0;
+
+            string[] input = Console.ReadLine().Split(' ');
+            int k = int.Parse(input[0]);
+            int s = int.Parse(input[1]);
+
+            for(int i=0; i<=k; i++)
+            {
+                    for(int j=0; j<=k; j++)
+                    {
+                        if(0<=s-i-j && s-i-j<=k) answer++;
+                    }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
