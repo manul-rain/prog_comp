eb2355f2a2fa78dd7fe0ca7333828969aecba2a0
manul-rain <kdr71.nkn32@gmail.com>
2019-10-22 19:53:26 +0900

    in the middle of RC061 AtCoder

---
+++ VisualStudioCode/AtCoder/RegularContest061/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegularContest061
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            int len = str.Length;
+
+            int num = int.Parse(str);
+            //it exists in all pattern 
+            //  when the number of 'plus' is zero.
+            int sum = num;
+
+            for()
+        }
+
+        class division
+        {
+            
+        }
+    }
+}
